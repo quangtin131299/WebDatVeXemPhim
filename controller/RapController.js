90ef174c9b007219b04349d86e9753d6d1f9bfae
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-04-11 23:08:40 +0700

    load rap phim

--- controller/RapController.js
+++ controller/RapController.js
@@ -3,7 +3,9 @@ class RapController{
 
     loadRap(req, res){
         rapModel.loadALLRap().then(function(result){
+
             res.render("Cinema/cinema", {listrap: result});
+            
         }).catch(function(err){
             res.render("Cinema/cinema"); 
         })
