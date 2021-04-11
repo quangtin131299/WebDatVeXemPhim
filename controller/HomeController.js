615acc564ef29c559c5b8f934f914e4d171eab4d
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-04-11 23:11:55 +0700

    load phim dang chieu

--- controller/HomeController.js
+++ controller/HomeController.js
@@ -5,7 +5,9 @@ class HomeController{
         let listPhim = [];
         phimModel.loadPhimDangChieu().then((result) => {
             listPhim = result; 
+
             res.render("Home/index", {listPhimDC: listPhim})
+            
         }).catch((err) => {
             res.render("Home/index");
         });        
